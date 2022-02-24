e9921df55c0557938290eac2e65277289f309092
anton <demeshkevichanton@gmail.com>
2022-02-24 17:41:44 +0300

    ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
-
 function App() {
   return (
-    <div>
-      Hello
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <svg />
+          <div className="headerInfo">
+            <h3>Sneakers</h3>
+            <p>Магазин кроссовок</p>
+          </div>
+        </div>
+
+        <ul>
+          <li>
+            <svg /> <span>1205p</span>
+          </li>
+          <li>
+            <svg />
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
